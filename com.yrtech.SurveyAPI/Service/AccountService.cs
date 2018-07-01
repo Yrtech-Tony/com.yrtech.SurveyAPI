6451bb9cd828a1282ba612d9ab59a5c204879921
Tony <moujunsheng15@163.com>
2018-07-02 07:25:33 +0800

    First commit

---
+++ com.yrtech.SurveyAPI/Service/AccountService.cs
@@ -0,0 +1,33 @@
+﻿using com.yrtech.SurveyAPI.Common;
+using com.yrtech.SurveyAPI.DTO.Account;
+using Survey.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace com.yrtech.SurveyAPI.Service
+{
+    public class AccountService
+    {
+        Entities db = new Entities();
+
+        public async Task<APIResult> Login(string accountId,string password)
+        {
+            SqlParameter[] para = new SqlParameter[] { new SqlParameter("@AccountId", accountId),
+                                                       new SqlParameter("@Password",password)};
+            Type t = typeof(AccountDto);
+            string sql = @"SELECT A.TenantId,C.BrandId,B.TenantCode,B.TenantName,D.BrandName,AccountId,AccountName,[Password],ISNULL(UseChk,0) AS UseChk 
+                            FROM UserInfo A INNER JOIN Tenant B ON A.TenantId = B.TenantId
+                                            INNER JOIN UserInfoBrand C ON A.Id = C.UserId
+                                            INNER JOIN Brand D ON C.BrandId = D.BrandId AND B.TenantId = D.TenantId
+                            WHERE AccountId = @AccountId AND[Password] = @Password
+                            AND UseChk = 1";
+            List<AccountDto> accountList =  db.Database.SqlQuery(t, sql, para).Cast<AccountDto>().ToList();
+            return new APIResult() { Status = true, Body = CommonHelper.EncodeDto<AccountDto>(accountList) };
+        }
+    }
+}
---
+++ com.yrtech.SurveyAPI/Service/DataDownService.cs
@@ -0,0 +1,22 @@
+﻿using com.yrtech.SurveyAPI.Common;
+using Survey.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Data.Entity;
+
+namespace com.yrtech.SurveyAPI.Service
+{
+    public class DataDownService
+    {
+        Entities db = new Entities();
+
+        public async Task<APIResult> GetAllTenant()
+        {
+            List<Tenant> tenant = await db.Tenant.ToListAsync();
+            return new APIResult() { Status = true, Body = CommonHelper.EncodeDto<Tenant>(tenant) };
+        }
+    }
+}
