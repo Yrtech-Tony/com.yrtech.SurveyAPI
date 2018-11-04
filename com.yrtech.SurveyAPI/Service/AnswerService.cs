7e26d05adb17e35ed36d2df8f48697ae2e7912ab
Tony <moujunsheng15@163.com>
2018-11-04 23:49:13 +0800

    修改了sql 语句

--- com.yrtech.SurveyAPI/Service/AnswerService.cs
+++ com.yrtech.SurveyAPI/Service/AnswerService.cs
@@ -403,7 +403,7 @@ namespace com.yrtech.SurveyAPI.Service
                            A.ShopId,A.AnswerId,A.InspectionStandardResult,A.FileResult,A.LossResult,A.ShopConsultantResult,
                            A.PhotoScore,A.Remark,A.InUserId,A.InDateTime,A.ModifyUserId,A.ModifyDateTime,A.UploadDate,A.UploadUserId
                            ,CASE WHEN (SELECT Count(*) FROM SubjectTypeScoreRegion WHERE SubjectId = S.SubjectId)=2 THEN '0' -- 交叉类
-                            CASE WHEN  (SELECT Count(*) FROM SubjectTypeScoreRegion WHERE SubjectId = S.SubjectId AND SubjectTypeId = 1)=1 THEN '1'-- 照片类
+                             WHEN  (SELECT Count(*) FROM SubjectTypeScoreRegion WHERE SubjectId = S.SubjectId AND SubjectTypeId = 1)=1 THEN '1'-- 照片类
                             ELSE '2' END AS SubjectTypeCode
                     FROM Subject S 
 		            LEFT JOIN Answer A ON S.ProjectId=A.ProjectId AND S.SubjectId = A.SubjectId
