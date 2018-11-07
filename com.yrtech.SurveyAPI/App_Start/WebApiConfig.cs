f1d3d658328b47447ed717f40f31e04f6a341eaa
huohaitao <huohaitao1@126.com>
2018-11-07 23:30:03 +0800

    添加跨域请求权限

--- com.yrtech.SurveyAPI/App_Start/WebApiConfig.cs
+++ com.yrtech.SurveyAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Net.Http;
+using Newtonsoft.Json.Serialization;
+using System.Configuration;
 
 namespace com.yrtech.SurveyAPI
 {
@@ -10,6 +14,13 @@ namespace com.yrtech.SurveyAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API 配置和服务
+ 
+            //跨域配置
+            var allowOrigins = ConfigurationManager.AppSettings["cors_allowOrigins"];
+            var allowHeaders = ConfigurationManager.AppSettings["cors_allowHeaders"];
+            var allowMethods = ConfigurationManager.AppSettings["cors_allowMethods"];
+            var globalCors = new EnableCorsAttribute(allowOrigins, allowHeaders, allowMethods);
+            config.EnableCors(globalCors);
 
             // Web API 路由
             config.MapHttpAttributeRoutes();
