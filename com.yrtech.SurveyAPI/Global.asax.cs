d5ba364f7c8c705de2e0146445443f4afaccdf94
huoht <huohaitao1@126.com>
2023-05-17 23:11:07 +0800

    feat: 增加quartz.net 定时任务执行

--- com.yrtech.SurveyAPI/Global.asax.cs
+++ com.yrtech.SurveyAPI/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using com.yrtech.SurveyAPI.Job;
+using Quartz;
+using Quartz.Impl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +15,17 @@ namespace com.yrtech.SurveyAPI
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            //1.创建作业调度池(Scheduler)
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            //2.创建一个具体的作业即job (具体的job需要单独在一个文件中执行)
+            var job = JobBuilder.Create<BakDataJob>().Build();
+            //3.创建并配置一个触发器即trigger   1s执行一次
+            var trigger = TriggerBuilder.Create().WithCronSchedule("0/1 * * * * ?").Build();
+            //4.将job和trigger加入到作业调度池中
+            scheduler.ScheduleJob(job, trigger);
+            //5.开启调度
+            scheduler.Start();
         }
     }
 }
---
+++ com.yrtech.SurveyAPI/Job/BakDataJob.cs
@@ -0,0 +1,19 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace com.yrtech.SurveyAPI.Job
+{
+    public class BakDataJob :IJob
+    {
+
+        public void Execute(IJobExecutionContext context)
+        {
+
+            Trace.WriteLine("Execute BakDataJob " + DateTime.Now.ToString());
+        }
+    }
+}
