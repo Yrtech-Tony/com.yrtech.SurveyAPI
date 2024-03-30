a7e269be5a2a9f7627051259604767ad6caff402
Tony <moujunsheng15@163.com>
2024-03-30 21:39:48 +0800

    添加了dto

---
+++ com.yrtech.SurveyAPI/DTO/Master/ChapterReportTypeDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace com.yrtech.SurveyAPI.DTO
+{
+    [Serializable]
+    public class ChapterReportTypeDto
+    {
+        public int ReportTypeId { get; set; }
+        public string ReportTypeCode { get; set; }
+        public string ReportTypeName { get; set; }
+        public decimal? FullScore { get; set; }
+        public int ProjectId { get; set; }
+        public int ChapterId { get; set; }
+        public string ChapterCode { get; set; }
+        public string ChapterName { get; set; }
+
+        public bool ImportChk { get; set; }
+        public string ImportRemark { get; set; }
+        public Nullable<int> InUserId { get; set; }
+
+
+    }
+}
---
+++ com.yrtech.SurveyAPI/DTO/Master/ReportTypeShopDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace com.yrtech.SurveyAPI.DTO
+{
+    [Serializable]
+    public class ReportTypeShopDto
+    {
+        public int ReportTypeId { get; set; }
+        public int ReportShopId { get; set; }
+        public string ReportTypeCode { get; set; }
+        public string ReportTypeName { get; set; }
+        public int ProjectId { get; set; }
+        public int ShopId { get; set; }
+        public string ShopCode { get; set; }
+        public string ShopName { get; set; }
+
+        public bool ImportChk { get; set; }
+        public string ImportRemark { get; set; }
+        public Nullable<int> InUserId { get; set; }
+
+
+    }
+}
